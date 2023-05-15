5bc463b77d572dbffe8ffbbf3214d81e0853f71f
yakorch <korchyar19@gmail.com>
2023-05-15 21:29:50 +0300

    some work not done

--- NMF/korch_sviat_letsgo.py
+++ NMF/korch_sviat_letsgo.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import NMF
 import wave
 
+
 def signal_to_wav(signal, framerate, filename):
     # save numpy array as wave file
     signal = signal.astype(np.int16)
@@ -15,33 +16,41 @@ def signal_to_wav(signal, framerate, filename):
     wave_file.writeframes(signal.tobytes())
     wave_file.close()
 
-# Load the mixed audio signal
-mix_file = "../AudioData/ICA_mix3.wav"
-y, sr = librosa.load(mix_file, sr=None)
 
-# Compute the short-time Fourier transform (STFT) of the mixed signal
-stft_y = librosa.stft(y)
+# Load both audio files
+file1 = "../AudioData/audio_13-14_X1.wav"
+file2 = "../AudioData/audio_13-14_X2.wav"
 
-# Calculate the magnitude spectrogram
-mag_y = np.abs(stft_y)
+y1, sr1 = librosa.load(file1, sr=None)
+y2, sr2 = librosa.load(file2, sr=None)
 
-# Apply NMF to the magnitude spectrogram
-n_sources = 3
+# Compute the short-time Fourier transform (STFT) for both signals
+stft_y1 = librosa.stft(y1)
+stft_y2 = librosa.stft(y2)
+
+# Calculate the magnitude spectrograms for both signals
+mag_y1 = np.abs(stft_y1)
+mag_y2 = np.abs(stft_y2)
+
+# Combine the magnitude spectrograms into a single matrix
+mag_y = np.concatenate((mag_y1, mag_y2), axis=1)
+
+# Apply NMF to the combined magnitude spectrogram
+n_sources = 2
 nmf = NMF(n_components=n_sources, random_state=42)
 W = nmf.fit_transform(mag_y)
 H = nmf.components_
 
 # Reconstruct the separated sources
-sources = np.zeros((n_sources, stft_y.shape[0], stft_y.shape[1]), dtype=np.complex64)
+sources = np.zeros((n_sources, stft_y1.shape[0], stft_y1.shape[1]), dtype=np.complex64)
 for i in range(n_sources):
-    sources[i] = (W[:, i:i+1] @ H[i:i+1, :]) * np.exp(1j * np.angle(stft_y))
+    sources[i] = (W[:, i:i + 1] @ H[i:i + 1, :stft_y1.shape[1]]) * np.exp(1j * np.angle(stft_y1))
+    sources[i] *= 10_000
 
 # Convert the time-frequency representation back to audio and save the separated sources
 for i in range(n_sources):
     y_i = librosa.istft(sources[i])
-    coeff = 10_000
-    y_i *= coeff
-    signal_to_wav(y_i, sr, f'separated_source_{i+1}.wav')
-
+    # y_i *= 10_000
+    signal_to_wav(y_i, sr1, f'separated_source_{i + 1}.wav')
 
 print("Done!")
